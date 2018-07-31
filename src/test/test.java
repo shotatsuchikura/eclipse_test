7093e8cf3cff6c5030a42ce939a1f0455c6c6db6
shotatsuchikura <shotatsuchikura@gmail.com>
2018-07-31 16:30:59 +0900

    change code

--- src/test/test.java
+++ src/test/test.java
@@ -4,7 +4,8 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		// TODO Auto-generated method stub
+		int tsuchikura = 1;
+		System.out.println(tsuchikura);
 
 	}
 
